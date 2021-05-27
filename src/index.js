8f6672108d7c538ea1f2586455b968a243f72503
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-27 15:28:08 -0500

    feat finish version 1.0

--- src/index.js
+++ src/index.js
@@ -2,37 +2,50 @@ import "./styles.css";
 import "./animation-text";
 
 const projects = document.querySelector(".projects");
+let url = "https://9dhcpnsgga.execute-api.us-east-2.amazonaws.com/default/personal-projects"
 
-function generateProject () {
+fetch(url)
+  .then(response => response.json())
+.then(data => {
+  data.map((project) => {
+    generateProject(project);
+  })
+})
+  .catch(console.log)
+
+function generateProject (data) {
   let project = `
     <div class="project__container">
-            <h2 class="project__name">Pelis Data</h2>
+            <h2 class="project__name">${data.title}</h2>
             <div class="project__description">
-                <img src="https://res.cloudinary.com/du9fag77u/image/upload/v1602903485/Pelis%20Data.png" alt="" class="project__description__image">
+                <img src="${data.image}" alt="${data.title}" class="project__description__image">
                 <div class="project__description__info">
-                    <p class="project__description__info__text">
-                        Aplicación donde puedes consultar información sobre películas,
-                        cuenta con registro y login de usuarios se uso firebase para ese fin,
-                        los datos vienen de la API de OMDb API
-                    </p>
+                    <p class="project__description__info__text">${data.description}</p>
                     <ul class="project__description__info__technologies">
-                        <li class="project__description__info__technologies__tech">JavaScript</li>
-                        <li class="project__description__info__technologies__tech">JavaScript</li>
-                        <li class="project__description__info__technologies__tech">JavaScript</li>
-                        <li class="project__description__info__technologies__tech">JavaScript</li>
-                        <li class="project__description__info__technologies__tech">JavaScript</li>
+                        ${generateSkills(data.technologies)}
                     </ul>
                 </div>
             </div>
             <div class="project__buttons">
-                <a class="project__buttons__demo" href="https://www.google.com">
+                <a class="project__buttons__demo" href="${data.code}">
                     <img src="./images/github.svg" alt="" class="project__buttons__item">
                 </a>
-                <a class="project__buttons__github" href="https://www.google.com">
+                <a class="project__buttons__github" href="${data.demo}">
                     <img src="./images/web2.svg" alt="" class="project__buttons__item">
                 </a>
             </div>
 
         </div>
   `;
+
+  projects.insertAdjacentHTML("beforeend", project);
+}
+
+function generateSkills(skills) {
+  let result = "";
+  skills.map((skill) => {
+    let data = `<li class="project__description__info__technologies__tech">${skill}</li>`
+    result += data;
+  })
+  return result;
 }
