9f801e043ae8380978d957b3547f529863683194
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2021-05-27 19:34:00 -0500

    fix effect loading images

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./animation-text";
 import generateProject from "./generate-project";
 
 const projects = document.querySelector(".projects");
+
 let url = "https://9dhcpnsgga.execute-api.us-east-2.amazonaws.com/default/personal-projects";
 
 fetch(url).then(response => response.json()).then(data => {
